18ef465783a667c31dc890824ce80c9dcbcf56f7
sagesaintsalem <ms.r.toussaint@gmail.com>
2024-06-03 23:06:59 +0100

    First draft with get and post requests running

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+let messages = [{"name": "John", "message":"Hey you"}, {"name": "Jimmy", "message":"See you"}, {"name": "Jacob", "message":"Fits daein?"}];
+
+app.get("/messages", (_request, result) => {
+    result.json(messages);
+})
+
+app.post("/messages", (request, result) => {
+    messages.unshift(request.body);
+    console.log(request.body);
+    result.json(request.body);
+})
+
+app.listen(5000, () => {console.log("Server started on port 5000")})

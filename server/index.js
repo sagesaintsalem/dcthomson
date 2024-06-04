29078c4be6088c34bb35efdc8a6714d5dca17b8d
sagesaintsalem <ms.r.toussaint@gmail.com>
2024-06-04 17:30:56 +0100

    postman tests added

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ let messages = [{"name": "John", "message":"Hey you"}, {"name": "Jimmy", "messag
 
 app.get("/messages", (_request, result) => {
     result.json(messages);
+    localStorage.setItem(messages);
 })
 
 app.post("/messages", (request, result) => {
     messages.unshift(request.body);
     console.log(request.body);
     result.json(request.body);
+    localStorage.setItem(messages);
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})

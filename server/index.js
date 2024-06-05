6a4eb70ec9adad0e467161ea54b033d3b94f680a
sagesaintsalem <ms.r.toussaint@gmail.com>
2024-06-05 10:58:47 +0100

    Get and Post fixed

--- server/index.js
+++ server/index.js
@@ -4,14 +4,22 @@ app.use(express.json());
 let messages = [{"name": "John", "message":"Hey you"}, {"name": "Jimmy", "message":"See you"}, {"name": "Jacob", "message":"Fits daein?"}];
 
 app.get("/messages", (_request, result) => {
-    result.json(messages);
-    localStorage.setItem(messages);
+    try {
+        result.json(messages);
+        localStorage.setItem(messages);
+    } catch (error){
+        console.error("Error: " + error)
+    }
 })
 
 app.post("/messages", (request, result) => {
-    messages.unshift(request.body);
-    result.json(request.body);
-    localStorage.setItem(messages);
+    try {
+        messages.unshift(request.body);
+        result.json(request.body);
+        localStorage.setItem(messages);
+    } catch (error){
+        console.error("Error: " + error)
+    }
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
